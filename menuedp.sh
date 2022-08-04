8633974e832132f99f46c0823ecfdf46dbaebc07
Alejo Lo Menzo <alejolomenzo@gmail.com>
2022-08-04 16:33:19 -0300

    Modifique el menu ya que me faltaban un script

--- menuedp.sh
+++ menuedp.sh
@@ -7,16 +7,24 @@ echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 PS3="Please select the script to run:"
 SCRIPTS=("findNames" "blankLinesCounter")
 
-select scr in "${SCRIPTS[@]}" Quit
+select scr in "${SCRIPTS[@]}" Exit
 do
     case $scr in
+	"statsWords.sh")
+	  ./statsWords.sh loren.txt
+	  ;;
+	"statsUsageWords.sh")
+	  ./statsUsageWords.sh loren.txt 
 	"findNames")
 	  ./findNames.sh loren.txt
+	  ;;
+	"statsSentences.sh")
+	  ./statsSentences.sh loren.txt
 	  ;;
 	"blankLinesCounter")
 	  ./blankLinesCounter.sh loren.txt
 	  ;;
-	"Quit")
+	"Exit")
 	  exit 0
 	  ;;
     esac
